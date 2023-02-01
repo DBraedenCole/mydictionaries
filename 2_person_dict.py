f30aff954debf68cbca62b1a6b3ce1d5ec078a6d
Braeden Cole <braeden_cole1@baylor.edu>
2023-01-31 22:20:41 -0600

    work in class 1/30

--- 2_person_dict.py
+++ 2_person_dict.py
@@ -6,20 +6,19 @@ person["spouse"] = "Edna"
 person["children"] = ["Ralph", "Betty", "Joey"]
 person["pets"] = {"dog": "Fido", "cat": "Sox"}
 
-#print(person)
+print(person)
 
 # print out the name of the second child
-
-
+print(person["children"][1])
 
 # print out the name of the cat
+print(person["pets"]["cat"])
 
+# iterate through all children and print out each child
+for c in person["children"]:
+    print(c)
 
-
-# use a loop to print out the names of each child
-
-
-
-
-# use a loop to print out the pets in the following format:
-# The type of pet is: dog and the name of the pet is: Fido
+# print out the pets in this format:
+# type of pet: dog name of pert: Fido
+for p, n in person["pets"].items():
+    print(f"type of pet: {p} name of pet {n}")
